86e4435dcaf773587e29a8539bed434e669e1c14
Andro55X <androxyz55@gmail.com>
2026-06-12 16:57:05 +0700

    add lod

--- music_assistant/providers/universal_group/player.py
+++ music_assistant/providers/universal_group/player.py
@@ -182,7 +182,9 @@ class UniversalGroupPlayer(Player):
         values: dict[str, ConfigValueType] | None = None,
     ) -> list[ConfigEntry]:
         """Return all (provider/player specific) Config Entries for the given player (if any)."""
-        self.logger.warning("UGP get_config_entries called")
+        self.logger.warning(
+          "UGP entries include: group_members,dynamic_members,disable_chunked,http_profile"
+        )
         return [
             # add universal group specific entries
             CONFIG_ENTRY_UGP_NOTE,
